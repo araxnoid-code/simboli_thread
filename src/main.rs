2ef7a8d75bbca1e5a9599092b03ccd0a0ba79b4f
araxnoid <araxnoid0@gmail.com>
2026-02-23 20:39:57 +0700

    ready to merge

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,29 @@ impl TaskTrait<MyOuput> for MyTask {
 fn main() {
     let thread_pool = SimboliThread::<MyTask, MyOuput, 8, 32>::init();
 
-    let wait_a = thread_pool.spawn_task(MyTask(|| {
-        sleep(Duration::from_millis(500));
-        println!("hello world");
-        MyOuput::String
-    }));
+    for i in 0..100 {
+        sleep(Duration::from_millis(25));
+    }
 
-    let wait_b = thread_pool.spawn_task(MyTask(|| {
-        sleep(Duration::from_millis(500));
-        println!("this different task!");
-        MyOuput::Int
-    }));
+    // let wait_a = thread_pool.spawn_task(MyTask(|| {
+    //     sleep(Duration::from_millis(500));
+    //     println!("hello world");
+    //     MyOuput::String
+    // }));
 
-    let wait_c = thread_pool.spawn_task(MyTask(|| {
-        sleep(Duration::from_millis(500));
-        println!("that's work!");
-        MyOuput::Int
-    }));
+    // let wait_b = thread_pool.spawn_task(MyTask(|| {
+    //     sleep(Duration::from_millis(500));
+    //     println!("this different task!");
+    //     MyOuput::Int
+    // }));
 
-    thread_pool.join();
+    // let wait_c = thread_pool.spawn_task(MyTask(|| {
+    //     sleep(Duration::from_millis(500));
+    //     println!("that's work!");
+    //     MyOuput::Int
+    // }));
 
-    println!("done!")
+    // thread_pool.join();
+
+    // println!("done!")
 }
