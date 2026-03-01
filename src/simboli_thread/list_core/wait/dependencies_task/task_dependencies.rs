4c63af878d8d23d9245f7f626ab2350d5ee8df25
araxnoid <araxnoid0@gmail.com>
2026-03-01 14:46:34 +0700

    error cause loop trait in ExecTask

--- src/simboli_thread/list_core/wait/dependencies_task/task_dependencies.rs
+++ src/simboli_thread/list_core/wait/dependencies_task/task_dependencies.rs
@@ -3,27 +3,32 @@ use std::{
     sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize},
 };
 
-use crate::{OutputTrait, TaskTrait, WaitingTask, simboli_thread::list_core::Waiting};
+use crate::{
+    OutputTrait, TaskTrait, TaskWithDependenciesTrait, WaitingTask,
+    simboli_thread::list_core::Waiting,
+};
 
 // will be shared. to Waiting<O> and WaitingTask<F, O>
-pub struct TaskDependenciesCore<F, O>
+pub struct TaskDependenciesCore<F, FD, O>
 where
     F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
     O: 'static + OutputTrait,
 {
     pub(crate) status: bool,
     pub(crate) done: AtomicBool,
     pub(crate) counter: AtomicUsize,
-    pub(crate) start: AtomicPtr<WaitingTask<F, O>>, // default null, will capture the task need this task output
-    pub(crate) end: AtomicPtr<WaitingTask<F, O>>, // default null, will capture the task need this task output
+    pub(crate) start: AtomicPtr<WaitingTask<F, FD, O>>, // default null, will capture the task need this task output
+    pub(crate) end: AtomicPtr<WaitingTask<F, FD, O>>, // default null, will capture the task need this task output
 }
 
-impl<F, O> TaskDependenciesCore<F, O>
+impl<F, FD, O> TaskDependenciesCore<F, FD, O>
 where
     F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
     O: 'static + OutputTrait,
 {
-    pub fn init(counter: usize) -> TaskDependenciesCore<F, O> {
+    pub fn init(counter: usize) -> TaskDependenciesCore<F, FD, O> {
         Self {
             status: true,
             done: AtomicBool::new(false),
@@ -33,7 +38,7 @@ where
         }
     }
 
-    pub fn blank() -> TaskDependenciesCore<F, O> {
+    pub fn blank() -> TaskDependenciesCore<F, FD, O> {
         Self {
             status: false,
             done: AtomicBool::new(false),
@@ -44,21 +49,23 @@ where
     }
 }
 
-pub struct TaskDependencies<F, O>
+pub struct TaskDependencies<F, FD, O>
 where
     F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
     O: 'static + OutputTrait,
 {
-    pub(crate) task_dependencies_ptr: &'static TaskDependenciesCore<F, O>,
+    pub(crate) task_dependencies_ptr: &'static TaskDependenciesCore<F, FD, O>,
     pub waiting_list: Vec<Waiting<O>>,
 }
 
-impl<F, O> TaskDependencies<F, O>
+impl<F, FD, O> TaskDependencies<F, FD, O>
 where
     F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
     O: 'static + OutputTrait,
 {
-    pub fn blank() -> TaskDependencies<F, O> {
+    pub fn blank() -> TaskDependencies<F, FD, O> {
         Self {
             task_dependencies_ptr: Box::leak(Box::new(TaskDependenciesCore::blank())),
             waiting_list: vec![],
--- src/simboli_thread/list_core/wait/waiting_task.rs
+++ src/simboli_thread/list_core/wait/waiting_task.rs
@@ -4,14 +4,15 @@ use crate::{
     TaskDependencies, simboli_thread::list_core::wait::dependencies_task::TaskDependenciesCore,
 };
 
-pub struct WaitingTask<F, O>
+pub struct WaitingTask<F, FD, O>
 where
     F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
     O: 'static + OutputTrait,
 {
     pub(crate) id: u64,
-    pub(crate) task: F,
-    pub(crate) next: AtomicPtr<WaitingTask<F, O>>,
+    pub(crate) task: ExecTask<F, FD, O>,
+    pub(crate) next: AtomicPtr<WaitingTask<F, FD, O>>,
     pub(crate) waiting_return_ptr: &'static AtomicPtr<O>,
     // dependencies
     pub(crate) task_dependencies_core_ptr: &'static TaskDependenciesCore<F, O>, // will be shared. to Waiting<O> and WaitingTask<F, O>
@@ -20,9 +21,28 @@ where
 
 pub trait OutputTrait {}
 
-pub enum ExecTask<F, TD> {
+pub enum ExecTask<F, FD, O>
+where
+    F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
+    O: 'static + OutputTrait,
+{
     Task(F),
-    TaskWithDependencies(TD),
+    TaskWithDependencies((FD, &'static TaskDependencies<F, FD, O>)),
+}
+
+impl<F, FD, O> ExecTask<F, FD, O>
+where
+    F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, O> + Send + 'static,
+    O: 'static + OutputTrait,
+{
+    pub fn exec(self) -> O {
+        match self {
+            ExecTask::Task(f) => f.exec(),
+            ExecTask::TaskWithDependencies((f, dependencies)) => f.exec(dependencies),
+        }
+    }
 }
 
 pub trait TaskTrait<O>
@@ -30,4 +50,13 @@ where
     O: OutputTrait,
 {
     fn exec(&self) -> O;
+}
+
+pub trait TaskWithDependenciesTrait<F, FD, O>
+where
+    F: TaskTrait<O> + Send + 'static,
+    FD: TaskWithDependenciesTrait<F, FD, O> + Send + 'static,
+    O: OutputTrait + 'static,
+{
+    fn exec(&self, task_dependencies: &'static TaskDependencies<F, FD, O>) -> O;
 }
